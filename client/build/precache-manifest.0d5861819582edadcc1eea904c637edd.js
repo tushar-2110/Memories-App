5ecfe111fcbc5f00460ffc7c1e14b70e5726fcff
Tushar <tusharsharma123456.k20@gmail.com>
2023-05-22 15:50:29 +0530

    created a build

---
+++ client/build/precache-manifest.0d5861819582edadcc1eea904c637edd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0dd4a573700dffb3fc922c2bb11349a2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "77b51f30f4a3e17fbe95",
+    "url": "/static/css/main.a1d481cb.chunk.css"
+  },
+  {
+    "revision": "20ae1808e1c9af56956c",
+    "url": "/static/js/2.bfb52602.chunk.js"
+  },
+  {
+    "revision": "90377550dd86ed303dd30d8f487872de",
+    "url": "/static/js/2.bfb52602.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "77b51f30f4a3e17fbe95",
+    "url": "/static/js/main.2593b870.chunk.js"
+  },
+  {
+    "revision": "693d8af27c1684c7fee9",
+    "url": "/static/js/runtime-main.20628b47.js"
+  },
+  {
+    "revision": "5c0c63fb62ec323a10598d7e9de4609e",
+    "url": "/static/media/memories.5c0c63fb.png"
+  }
+]);
